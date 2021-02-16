1b2c8bda1f2a05aceb43b12d39673e4983440df8
Mr SparX <71897067+MrSp4rX@users.noreply.github.com>
2021-02-16 10:15:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -123,5 +123,5 @@ def inbound_message():
     return ''
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=80, debug=True)
                             
